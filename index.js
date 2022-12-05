463a2cb30e556b4c4ce127a85e14abbee86121f3
Dan <dan@CodePC.localdomain>
2022-12-05 16:55:06 +0800

    npm updates

---
+++ index.js
@@ -0,0 +1,2 @@
+export {default as RefreshTimer} from './src/RefreshTimer';
+export * from './src/RefreshTimer';
--- src/index.js
+++
@@ -1,2 +0,0 @@
-export {default as RefreshTimer} from './RefreshTimer';
-export * from './RefreshTimer';
